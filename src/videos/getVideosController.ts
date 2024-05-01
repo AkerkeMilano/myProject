c152fb08aa04bbc77322bf61aa5743a8141200c3
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2024-05-01 10:47:02 +0600

    get video

--- src/videos/getVideosController.ts
+++ src/videos/getVideosController.ts
@@ -1,10 +1,11 @@
-import {Request, Response} from 'express'
 import {db} from '../db/db'
+import { Request, Response } from "express"
+import { HTTP_STATUSES } from '../settings'
 //import {OutputVideoType} from '../input-output-types/video-types'
  
-export const getVideosController = (req: Request, res: Response<any[]>) => {
-    res
-        .status(200)
-        .json(db.videos)
+
+export const getVideosController = (req: Request, res: Response): void => {
+    res.status(HTTP_STATUSES.OK_200).json(db.videos)
     console.log("video: ", db.videos)
 }
+ 
