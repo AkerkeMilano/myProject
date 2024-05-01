e456fe5257dda9046349642e254cbea2ce9b5503
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2024-05-01 11:11:15 +0600

    added type

--- src/videos/getVideosController.ts
+++ src/videos/getVideosController.ts
@@ -1,10 +1,11 @@
 import {db} from '../db/db'
 import { Request, Response } from "express"
 import { HTTP_STATUSES } from '../settings'
+import { InputVideoType } from '../db/video-db-type'
 //import {OutputVideoType} from '../input-output-types/video-types'
  
 
-export const getVideosController = (req: Request, res: Response): void => {
+export const getVideosController = (req: Request, res: Response<InputVideoType[]>): void => {
     res.status(HTTP_STATUSES.OK_200).json(db.videos)
 }
  
